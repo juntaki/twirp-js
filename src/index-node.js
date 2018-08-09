c0c42349feb402be676c71d2830199d3ad7d6257
juntaki <me@juntaki.com>
2018-08-09 15:40:51 +0900

    Fix deserialize

--- src/index-node.js
+++ src/index-node.js
@@ -5,7 +5,11 @@ var client = require("./client");
 var fetch = require("node-fetch");
 
 const serialize = msg => Buffer.from(msg.serializeBinary());
-const deserialize = responseType => res => res.buffer()
-    .then(b => responseType.deserializeBinary(new Uint8Array(b)).toObject());
-
+const deserialize = function (responseType) {
+  return function (res) {
+    return res.arrayBuffer().then(function (buf) {
+      return responseType.deserializeBinary(new Uint8Array(buf)).toObject();
+    });
+  };
+};
 module.exports = client.clientFactory(fetch, serialize, deserialize);
